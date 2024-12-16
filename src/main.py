19aac7e074d308382e02e16c478e70c825720974
Aybars Nazlica <aybarsnazlica@gmail.com>
2024-12-16 20:36:09 +0900

    Initial commit

---
+++ src/main.py
@@ -0,0 +1,14 @@
+from  textnode import TextNode, TextType
+
+
+def main():
+    tn = TextNode(
+        "This is a text node",
+        TextType.BOLD,
+        "https://www.boot.dev"
+    )
+    print(tn)
+
+
+if __name__ == "__main__":
+    main()
