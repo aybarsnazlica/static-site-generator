5f26750efc50c4a70fa0bfb9f921941534a5bf7d
Aybars Nazlica <aybarsnazlica@gmail.com>
2024-12-31 00:13:29 +0900

    Convert text node to html node

--- src/textnode.py
+++ src/textnode.py
@@ -1,10 +1,12 @@
 from enum import Enum
 
+from src.htmlnode import LeafNode
+
 
 class TextType(Enum):
-    NORMAL = "normal"
+    TEXT = "text"
     BOLD = "bold"
-    ITAL = "italic"
+    ITALIC = "italic"
     CODE = "code"
     LINK = "link"
     IMAGE = "image"
@@ -17,7 +19,29 @@ class TextNode:
         self.url = url
 
     def __eq__(self, other):
-        return self.text == other.text and self.text_type == other.text_type and self.url == other.url
+        return (
+                self.text == other.text
+                and self.text_type == other.text_type
+                and self.url == other.url
+        )
 
     def __repr__(self):
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+
+def text_node_to_html_node(text_node):
+    match text_node.text_type:
+        case TextType.TEXT:
+            return LeafNode(None, "")
+        case TextType.BOLD:
+            return LeafNode("b", text_node.text)
+        case TextType.ITALIC:
+            return LeafNode("i", text_node.text)
+        case TextType.CODE:
+            return LeafNode("code", text_node.text)
+        case TextType.LINK:
+            return LeafNode("a", text_node.text, {"href": text_node.url})
+        case TextType.IMAGE:
+            return LeafNode("img", text_node.text, {"src": text_node.url, "alt": text_node.text})
+
+    raise ValueError("not a text node type")
