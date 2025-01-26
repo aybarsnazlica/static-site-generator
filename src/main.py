6f718c0c35f3b0bccc74009c7d3bb3601dbc817a
Aybars Nazlica <aybarsnazlica@gmail.com>
2025-01-27 00:07:16 +0900

    Copy static to public

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 from textnode import TextNode, TextType
+from util import copy_content
 
 
 def main():
     tn = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
     print(tn)
+    copy_content("./static", "./public")
 
 
 if __name__ == "__main__":
