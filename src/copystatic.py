44cc3dfa5165310f51b5310bfd41bd3ded57865a
Aybars Nazlica <aybarsnazlica@gmail.com>
2025-03-21 00:09:22 +0900

    Use pathlib

--- src/copystatic.py
+++ src/copystatic.py
@@ -1,16 +1,19 @@
-import os
 import shutil
 
 
 def copy_files_recursive(source_dir_path, dest_dir_path):
-    if not os.path.exists(dest_dir_path):
-        os.mkdir(dest_dir_path)
+    if not source_dir_path.is_dir():
+        return
+    if not dest_dir_path.exists():
+        dest_dir_path.mkdir(parents=True, exist_ok=True)
 
-    for filename in os.listdir(source_dir_path):
-        from_path = os.path.join(source_dir_path, filename)
-        dest_path = os.path.join(dest_dir_path, filename)
+    for filename in source_dir_path.iterdir():
+        from_path = source_dir_path / filename.name
+        dest_path = dest_dir_path / filename.name
+
         print(f" * {from_path} -> {dest_path}")
-        if os.path.isfile(from_path):
+
+        if from_path.is_file():
             shutil.copy(from_path, dest_path)
         else:
             copy_files_recursive(from_path, dest_path)
--- src/main.py
+++ src/main.py
@@ -1,32 +1,32 @@
-import os
 import shutil
 import sys
-
+from pathlib import Path
 from copystatic import copy_files_recursive
 from generate_content import generate_pages_recursive
 
-
-dir_path_static = "./static"
-dir_path_public = "./docs"
-dir_path_content = "./content"
-template_path = "./template.html"
-default_basepath = "/"
+default_base_path = Path("/")
+dir_path_static = Path("static")
+dir_path_public = Path("public")
+dir_path_content = Path("content")
+template_path = Path("template.html")
 
 
 def main():
-    basepath = default_basepath
+    base_path = default_base_path
+
     if len(sys.argv) > 1:
-        basepath = sys.argv[1]
+        base_path = Path(sys.argv[1])
 
     print("Deleting public directory...")
-    if os.path.exists(dir_path_public):
+    if dir_path_public.exists():
         shutil.rmtree(dir_path_public)
 
     print("Copying static files to public directory...")
     copy_files_recursive(dir_path_static, dir_path_public)
 
     print("Generating content...")
-    generate_pages_recursive(dir_path_content, template_path, dir_path_public, basepath)
+    generate_pages_recursive(str(dir_path_content), str(template_path), str(dir_path_public), str(base_path))
 
 
-main()
+if __name__ == "__main__":
+    main()
